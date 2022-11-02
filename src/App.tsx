27e171b10e3307d1c71c323bcd9b86baca1f2264
ArtCorner <info@artcorner.hu>
2022-11-02 18:28:00 +0100

    add and setup tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "./App.css";
 import Drivers from "./pages/drivers";
 import Home from "./pages/home";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
