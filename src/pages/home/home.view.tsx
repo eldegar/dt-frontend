be327eb57a3a40f4039d95d79d183565d00a781d
ArtCorner <info@artcorner.hu>
2022-11-02 23:27:33 +0100

    add content to home page

--- src/pages/home/home.view.tsx
+++ src/pages/home/home.view.tsx
@@ -1,10 +1,20 @@
 import { ReactNode } from "react";
+import { Link } from "react-router-dom";
 
 type Props = {
   children?: ReactNode;
 };
 
 const HomePage = ({ children }: Props) => {
-  return <div>Kezdőlap</div>;
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <Link
+        className="text-4xl text-primary-600 hover:text-primary-900"
+        to="/drivers"
+      >
+        Go To Drivers!
+      </Link>
+    </div>
+  );
 };
 export default HomePage;
