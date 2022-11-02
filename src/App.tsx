b1673b9d347d9269065efa491ff4713dc5c4327e
ArtCorner <info@artcorner.hu>
2022-11-02 17:24:02 +0100

    setup router and react query

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,19 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Drivers from "./pages/drivers";
 import Home from "./pages/home";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 function App() {
+  const queryClient = new QueryClient();
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/drivers" element={<Drivers />} />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/drivers" element={<Drivers />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
