9f690fff438cb30c02b14d17915d59e3dd10a14f
ArtCorner <info@artcorner.hu>
2022-11-02 19:38:32 +0100

    add driver card componet

---
+++ src/components/driver-card/driver-card.view.tsx
@@ -0,0 +1,26 @@
+import { ReactNode } from "react";
+import { axiosInstance } from "../../lib/xhr";
+import { Driver } from "../../types/driver";
+
+interface Props extends Driver {}
+
+const DriverCard = (props: Props) => {
+  return (
+    <div className="flex relative">
+      <img
+        className="mr-3 w-36"
+        src={`${import.meta.env.VITE_API}${props.imgUrl}`}
+      ></img>
+      <div className="w-56">
+        <p>First Name: {props.firstname}</p>
+        <p>Last Name: {props.lastname}</p>
+        <p>Country: {props.country} </p>
+        <p>Code: {props.code} </p>
+      </div>
+      <div className="absolute top-0 left-0 text-xl font-semibold">
+        {props.place}
+      </div>
+    </div>
+  );
+};
+export default DriverCard;
