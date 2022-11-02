2c83783c22904c1af2b7f714da17e53fbce9c5c4
ArtCorner <info@artcorner.hu>
2022-11-02 19:37:26 +0100

    extend tailwind theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true,
+        padding: "2rem",
+      },
+      colors: {
+        primary: colors.green,
+        secondary: colors.sky,
+      },
+    },
   },
   plugins: [],
 };
